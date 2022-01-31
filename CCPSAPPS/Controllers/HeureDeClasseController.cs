8766c0e1aa33a32a849b8810e6f77cc4ebca8bd6
Richcard Larose <lrichcard@gmail.com>
2022-01-31 11:28:01 -0800

    HeureDeClasse is implemented, A change was made in the name of the table in the sqlserver app

--- CCPSAPPS/Controllers/HeureDeClasseController.cs
+++ CCPSAPPS/Controllers/HeureDeClasseController.cs
@@ -1,93 +1,114 @@
-﻿//using CCPSAPPS.Models;
-//using Microsoft.AspNetCore.Mvc;
-//using ApplicationDbContext = CCPSAPPS.Data.ApplicationDbContext;
+﻿using CCPSAPPS.Models;
+using Microsoft.AspNetCore.Mvc;
+using ApplicationDbContext = CCPSAPPS.Data.ApplicationDbContext;
 
-//namespace CCPSAPPS.Controllers
-//{
-//    public class HeureDeClasseController : Controller
-//    {
-//        private readonly ApplicationDbContext _db;
+namespace CCPSAPPS.Controllers
+{
+    public class HeureDeClasseController : Controller
+    {
+        private readonly ApplicationDbContext _db;
 
-//        public HeureDeClasseController(ApplicationDbContext db)
-//        {
-//            _db = db;
-//        }
+        public HeureDeClasseController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
 
-//        // GET: HeureDeClasseController
-//        public ActionResult Index()
-//        {
-//            IEnumerable<HeuresDeClass> heureDeClasse = _db.HeureDeClasses;
-//            return View(heureDeClasse);
-            
-//        }
+        // GET: HeureDeClasseController
+        public ActionResult Index()
+        {
+            IEnumerable<HeuresDeClass> heureDeClasse = _db.HeureDeClasses;
+            return View(heureDeClasse);
 
-//        //// GET: HeureDeClasseController/Details/5
-//        //public ActionResult Details(int id)
-//        //{
-//        //    return View();
-//        //}
+        }
 
-//        //// GET: HeureDeClasseController/Create
-//        //public ActionResult Create()
-//        //{
-//        //    return View();
-//        //}
+        //Get - Create
+        public IActionResult Create()
+        {
 
-//        //// POST: HeureDeClasseController/Create
-//        //[HttpPost]
-//        //[ValidateAntiForgeryToken]
-//        //public ActionResult Create(IFormCollection collection)
-//        //{
-//        //    try
-//        //    {
-//        //        return RedirectToAction(nameof(Index));
-//        //    }
-//        //    catch
-//        //    {
-//        //        return View();
-//        //    }
-//        //}
+            return View();
+        }
 
-//        //// GET: HeureDeClasseController/Edit/5
-//        //public ActionResult Edit(int id)
-//        //{
-//        //    return View();
-//        //}
+        //Post- Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(HeuresDeClass heureDeClasse)
+        {
+            _db.HeureDeClasses.Add(heureDeClasse);
+            _db.SaveChanges();
 
-//        //// POST: HeureDeClasseController/Edit/5
-//        //[HttpPost]
-//        //[ValidateAntiForgeryToken]
-//        //public ActionResult Edit(int id, IFormCollection collection)
-//        //{
-//        //    try
-//        //    {
-//        //        return RedirectToAction(nameof(Index));
-//        //    }
-//        //    catch
-//        //    {
-//        //        return View();
-//        //    }
-//        //}
+            return RedirectToAction("Index");
+        }
 
-//        //// GET: HeureDeClasseController/Delete/5
-//        //public ActionResult Delete(int id)
-//        //{
-//        //    return View();
-//        //}
+        //show Methode/ Details
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var heureClasse = _db.HeureDeClasses.Find(id);
+            if (heureClasse == null)
+            {
+                return NotFound();
+            }
+            return View(heureClasse);
+        }
 
-//        //// POST: HeureDeClasseController/Delete/5
-//        //[HttpPost]
-//        //[ValidateAntiForgeryToken]
-//        //public ActionResult Delete(int id, IFormCollection collection)
-//        //{
-//        //    try
-//        //    {
-//        //        return RedirectToAction(nameof(Index));
-//        //    }
-//        //    catch
-//        //    {
-//        //        return View();
-//        //    }
-//        //}
-//    }
-//}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Update(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var heureDeClasse = _db.HeureDeClasses.Find(id);
+            if (heureDeClasse == null)
+            {
+                return NotFound();
+            }
+            return View(heureDeClasse);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(HeuresDeClass heuresDeClass)
+        {
+            _db.HeureDeClasses.Update(heuresDeClass);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+
+        // GET: /Movies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            HeuresDeClass heureDeClasse = _db.HeureDeClasses.Find(id);
+            if (heureDeClasse == null)
+            {
+                return NotFound();
+            }
+            return View(heureDeClasse);
+        }
+
+
+
+        // POST: /Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            HeuresDeClass heureDeClasse = _db.HeureDeClasses.Find(id);
+            _db.HeureDeClasses.Remove(heureDeClasse);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
