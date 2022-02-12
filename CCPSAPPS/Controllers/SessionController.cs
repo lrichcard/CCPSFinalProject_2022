74e9a0d92605fe80097d48c6e3832ff4c8b700b9
Richcard Larose <lrichcard@gmail.com>
2022-02-11 22:06:14 -0800

    creating the session controller and the views index and create

---
+++ CCPSAPPS/Controllers/SessionController.cs
@@ -0,0 +1,39 @@
+﻿using CCPSAPPS.Models;
+using Microsoft.AspNetCore.Mvc;
+using ApplicationDbContext = CCPSAPPS.Data.ApplicationDbContext;
+namespace CCPSAPPS.Controllers
+{
+    public class SessionController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SessionController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Session> session = _db.Sessions;
+            return View(session);
+        }
+
+        //Get - Create
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        //Post- Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Session session)
+        {
+            _db.Sessions.Add(session);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+
