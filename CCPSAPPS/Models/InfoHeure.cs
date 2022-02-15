18ac05cf4ed02953ca49390a0ec268896b4d8a5c
Richcard Larose <lrichcard@gmail.com>
2022-02-15 04:48:11 -0800

    methode create de la table session est complete pour subir le design, Il reste a faire des petite modifcation dans les methodes update et details

---
+++ CCPSAPPS/Models/InfoHeure.cs
@@ -0,0 +1,8 @@
+﻿namespace CCPSAPPS.Models
+{
+    public class InfoHeure
+    {
+        public int HeureId { get; set; }
+        public string? NomHeure { get; set; }
+    }
+}
---
+++ CCPSAPPS/Models/MultipleInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace CCPSAPPS.Models
+{
+    public class MultipleInfo
+    {
+            //public int ClasseID { get; set; }
+            //public string? ClasseNom { get; set; }
+            public int? PersonneID { get; set; }
+            public string? PersonneNomComplet { get; set; }
+            //public string? jourDeRencontre { get; set; }
+            //public string? HeureDeClasse { get; set; }
+
+
+
+        
+    }
+}
